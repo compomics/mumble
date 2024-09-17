a5ac7e17067623b012933145eed5e81f6e7e534a
Jasper Van den Hemel <jasper.vandenhemel@ugent.be>
2024-09-17 14:59:39 +0200

    added cache for unimod data and combinations

--- mumble/mumble.py
+++ mumble/mumble.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 import logging
 import itertools
+import os
+import pickle
 from collections import namedtuple
 from pathlib import Path
 
@@ -356,7 +358,29 @@ class _ModificationHandler:
         """
         # TODO add amino acid variations (mutation) as flag
         self.combination_length = combination_length
-        self.get_unimod_database(exclude_mutations)
+        self.exclude_mutations = exclude_mutations
+        
+        cache_file = self._get_cache_file_path()
+
+        if os.path.exists(cache_file):
+            print("using cache")
+            with open(cache_file, 'rb') as f:
+                cache_data = pickle.load(f)
+            self.modification_df = cache_data['modification_df']
+            self.monoisotopic_masses = cache_data['monoisotopic_masses']
+            self.modifications_names = cache_data['modifications_names']
+        else:
+            self.get_unimod_database()
+            self.monoisotopic_masses, self.modifications_names = self._generate_modifications_combinations_lists(self.combination_length)
+            cache_data = {
+                'modification_df': self.modification_df,
+                'monoisotopic_masses': self.monoisotopic_masses,
+                'modifications_names': self.modifications_names
+            }
+            print("creating cache")
+            with open(cache_file, 'wb') as f:
+                pickle.dump(cache_data, f)
+
         if add_aa_combinations:
             if not fasta_file:
                 raise ValueError("Fasta file is required to add amino acid combinations")
@@ -428,9 +452,6 @@ class _ModificationHandler:
                 #"rounded_mass",
             ],
         )
-        
-        self.monoisotopic_masses, self.modifications_names = self._generate_modifications_combinations_lists(self.combination_length)
-        
 
 
     def _generate_modifications_combinations_lists(self, combination_length=1):
@@ -484,6 +505,23 @@ class _ModificationHandler:
         else:
             return [], []
 
+    def _get_cache_file_path(self):
+        """
+        Get path to cache file for combinations of modifications
+        
+        return:
+            str: path to cache file
+        """
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        parent_dir = os.path.dirname(current_dir)
+        cache_dir = os.path.join(parent_dir, "modification_cache")
+        
+        # Create the cache directory if it doesn't exist
+        os.makedirs(cache_dir, exist_ok=True)
+        
+        cache_file = os.path.join(cache_dir, f"length_{self.combination_length}_exclude_mutations_{self.exclude_mutations}.pkl")
+        
+        return cache_file
 
     def _get_name_to_mass_residue_dict(self):
         """
